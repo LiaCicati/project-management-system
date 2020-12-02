edd59ab4c50ee1e00d42545aebb5bd169d8b1574
Lori <loredanacicati@gmail.com>
2020-12-02 11:46:06 +0100

    added Task class

---
+++ SingleUserSystem/src/Model/Task.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Task
+{
+  private int ID;
+  private String title;
+  private double estimatedTime;
+  private double timeSpent;
+  private String description;
+  public static final String NOT_STARTED = "Not Started";
+  public static final String STARTED = "Started";
+  public static final String ENDED = "Ended";
+
+}
