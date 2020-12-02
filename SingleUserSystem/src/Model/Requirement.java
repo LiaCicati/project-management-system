24536b16cc9904859c79103e5b919281562a82e0
Lia <liacicati@gmail.com>
2020-12-02 11:43:14 +0100

    added Requirement class

---
+++ SingleUserSystem/src/Model/Requirement.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Requirement
+{
+  private int ID;
+  private double estimatedTime;
+  public static final String NOT_STARTED = "Not started";
+  public static final String STARTED = "Started";
+  public static final String ENDED = "Ended";
+  public static final String APPROVED = "Approved";
+  public static final String REJECTED = "Rejected";
+
+}
