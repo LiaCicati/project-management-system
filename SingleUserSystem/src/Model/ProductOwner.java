4fbb0069142a59fa787dc21aa3df5d965cf88b00
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-02 15:51:37 +0100

    Update ProductOwner.java

--- SingleUserSystem/src/Model/ProductOwner.java
+++ SingleUserSystem/src/Model/ProductOwner.java
@@ -1,13 +1,10 @@
 package Model;
-public class ProductOwner
+public class ProductOwner extends TeamMember
 {
-  private Name name;
-  private int id;
 
   public ProductOwner(Name name,int id)
   {
-    this.name=name;
-    this.id=id;
+   super(name,id);
   }
 
   public String getRole()
