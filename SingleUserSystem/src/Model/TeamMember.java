5154bf721d339bef3fceb6dda8a223fc026f86b9
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-03 10:44:25 +0100

    add some comments to the TeamMember class

--- SingleUserSystem/src/Model/TeamMember.java
+++ SingleUserSystem/src/Model/TeamMember.java
@@ -1,36 +1,66 @@
 package Model;
 
+/**
+ * @author Siyu Xia
+ * @version 3/12/2020
+ */
 public class TeamMember
 {
   private int id;
   private Name name;
 
+  /**
+   *Contructor for the teammember
+   * @param name the name of the teammember
+   * @param id the id of the teammember
+   */
   public TeamMember(Name name, int id)
   {
     this.name = name;
     setId(id);
   }
 
+  /**
+   *Getter for the teammember's name
+   * @return the name of the teammember
+   */
   public Name getName()
   {
     return name;
   }
 
+  /**
+   * Getter for the teammember's id
+   * @return the id of the teammember
+   */
   public int getId()
   {
     return id;
   }
 
+  /**
+   * Sets the id of a teammember
+   * @param id the id
+   */
   public void setId(int id)
   {
     this.id = id;
   }
-  
+
+  /**
+   * Sets the name of a teammember
+   * @param name the name
+   */
   public void setName(Name name)
   {
     this.name=name;
   }
 
+  /**
+   * Checking if two teammembers are the same
+   * @param obj the teammember compared to
+   * @return true if the teammembers are the same or false if not
+   */
   public boolean equals(Object obj)
   {
     if (!(obj instanceof TeamMember))
@@ -41,6 +71,10 @@ public class TeamMember
     return id == other.id && name.equals(other.name);
   }
 
+  /**
+   * Getting the information about a teammember
+   * @return a string with all needed information about a teammember
+   */
   public String toString()
   {
     return "ID: " + id + ", Full name: " + name;
