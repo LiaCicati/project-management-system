1c656320c42cea67e64508ff8ff4cc7a0eea4ce5
Lia <liacicati@gmail.com>
2020-12-03 10:22:09 +0100

    javadoc

--- SingleUserSystem/docs/package-search-index.js
+++ SingleUserSystem/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Mediator"},{"l":"Model"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Mediator"},{"l":"Model"},{"l":"View"}]
