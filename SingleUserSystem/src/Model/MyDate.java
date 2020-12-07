5b5180ace919403fd445a4d724bc5a51158dd31e
Lori <loredanacicati@gmail.com>
2020-12-07 15:32:42 +0100

    Auto stash before merge of "main" and "origin/main"

--- SingleUserSystem/src/Model/MyDate.java
+++ SingleUserSystem/src/Model/MyDate.java
@@ -3,13 +3,25 @@ package Model;
 import java.util.InputMismatchException;
 import java.util.Objects;
 
+
+/**
+ * @version v.2 7th December 2020
+ */
+
 public class MyDate
 {
   private int day;
   private int month;
   private int year;
 
-  public MyDate(int day,int month,int year)
+  /**
+   * Constructor for the date
+   *
+   * @param day   the day
+   * @param month the month
+   * @param year  the year
+   */
+  public MyDate(int day, int month, int year)
   {
     try
     {
@@ -23,27 +35,52 @@ public class MyDate
     }
   }
 
+  /**
+   * Getter for the day
+   *
+   * @return the day
+   */
   public int getDay()
   {
     return day;
   }
 
+  /**
+   * Getter for the month
+   *
+   * @return the month
+   */
   public int getMonth()
   {
     return month;
   }
 
+  /**
+   * Getter for the year
+   *
+   * @return the year
+   */
   public int getYear()
   {
     return year;
   }
 
+  /**
+   * A boolean method checking if it's a leap year
+   *
+   * @return true if it is a leap year and false if not
+   */
   public boolean isLeapYear()
   {
     return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
   }
 
-  public int numberOfDaysinMonth()
+  /**
+   * Getting the number of days in a month
+   *
+   * @return the number of days
+   */
+  public int numberOfDaysInMonth()
   {
     switch (month)
     {
@@ -63,6 +100,13 @@ public class MyDate
     }
   }
 
+  /**
+   * Setting the deadline
+   *
+   * @param day   the day
+   * @param month the month
+   * @param year  the year
+   */
   public void set(int day, int month, int year)
   {
     if (year < 0)
@@ -83,84 +127,120 @@ public class MyDate
     {
       day = 1;
     }
-    else if (day > numberOfDaysinMonth())
+    else if (day > numberOfDaysInMonth())
     {
-      day = numberOfDaysinMonth();
+      day = numberOfDaysInMonth();
     }
     this.day = day;
   }
 
+  /**
+   * Getting the name of the month
+   *
+   * @return the month name
+   */
   public String getMonthName()
   {
-    String status = "";
-    if (month == 1)
+    switch (month)
     {
-      status = "January";
+      case 1:
+        return "January";
+      case 2:
+        return "February";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "July";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "October";
+      case 11:
+        return "November";
+      case 12:
+        return "December";
+      default:
+        return "Error";
     }
-    else if (month == 2)
-    {
-      status = "February";
-    }
-    else if (month == 3)
-    {
-      status = "March";
-    }
-    else if (month == 4)
-    {
-      status = "April";
-    }
-    else if (month == 5)
-    {
-      status = "May";
-    }
-    else if (month == 6)
-    {
-      status = "June";
-    }
-    else if (month == 7)
-    {
-      status = "July";
-    }
-    else if (month == 8)
-    {
-      status = "Augest";
-    }
-    else if (month == 9)
-    {
-      status = "September";
-    }
-    else if (month == 10)
-    {
-      status = "October";
-    }
-    else if (month == 11)
-    {
-      status = "November";
-    }
-    else
-    {
-      status = "December";
-    }
-    return status;
   }
 
+  /**
+   * Getting the month number
+   *
+   * @param monthName the month name
+   * @return the month number
+   */
+  public int convertToMonthNumber(String monthName)
+  {
+    switch (monthName)
+    {
+      case "January":
+        return 1;
+      case "February":
+        return 2;
+      case "March":
+        return 3;
+      case "April":
+        return 4;
+      case "May":
+        return 5;
+      case "June":
+        return 6;
+      case "July":
+        return 7;
+      case "August":
+        return 8;
+      case "September":
+        return 9;
+      case "October":
+        return 10;
+      case "November":
+        return 11;
+      case "December":
+        return 12;
+      default:
+        return 1;
+    }
+  }
+
+  /**
+   * Creating a new instance of the class of current object
+   * @return the new instance of MyDate object
+   */
   public MyDate copy()
   {
-    MyDate other = new MyDate(day,month,year);
+    MyDate other = new MyDate(day, month, year);
     return other;
   }
 
+  /**
+   * Checking if two dates are the same
+   * @param obj object to be compared to
+   * @return a boolean indicating if two instances are similar
+   */
   public boolean equals(Object obj)
   {
-    if(!(obj instanceof MyDate))
+    if (!(obj instanceof MyDate))
     {
       return false;
     }
-    MyDate other=(MyDate)obj;
+    MyDate other = (MyDate) obj;
     return day == other.day && month == other.month && year == other.year;
   }
 
-  public String toString ()
+  /**
+   * Getting all the needed information about the created date
+   * @return a string containing the date
+   */
+  public String toString()
   {
     String s = "";
     if (day < 10)
@@ -175,58 +255,5 @@ public class MyDate
     s += month + "/" + year;
     return s;
   }
-  
-  public int convertToMonthNumber(String monthName)
-  {
-      int status = 0;
-      if (monthName == "January")
-      {
-        status = 1;
-      }
-      else if (monthName == "February")
-      {
-        status = 2;
-      }
-      else if (monthName == "March")
-      {
-        status = 3;
-      }
-      else if (monthName == "April")
-      {
-        status = 4;
-      }
-      else if (monthName == "May")
-      {
-        status = 5;
-      }
-      else if (monthName == "June")
-      {
-        status = 6;
-      }
-      else if (monthName == "July")
-      {
-        status = 7;
-      }
-      else if (monthName == "Augest")
-      {
-        status = 8;
-      }
-      else if (monthName == "September")
-      {
-        status = 9;
-      }
-      else if (monthName == "October")
-      {
-        status = 10;
-      }
-      else if (monthName == "November")
-      {
-        status = 11;
-      }
-      else
-      {
-        status = 12;
-      }
-      return status;
-    }
 }
+  
--- SingleUserSystem/src/Model/Name.java
+++ SingleUserSystem/src/Model/Name.java
@@ -1,41 +1,75 @@
 package Model;
 
+/**
+ * @version v.2 7th December 2020
+ */
 public class Name
 {
   private String firstName;
   private String lastName;
 
+  /**
+   * Constructor for the name of a team member
+   * @param firstName the first name of team member
+   * @param lastName the last name of team member
+   */
   public Name(String firstName, String lastName)
   {
     setName(firstName, lastName);
   }
 
+  /**
+   * Getter for the first name of a team member
+   * @return the first name
+   */
   public String getFirstName()
   {
     return firstName;
   }
 
+  /**
+   * Getter for the last name of a team member
+   * @return the last name
+   */
   public String getLastName()
   {
     return lastName;
   }
 
+  /**
+   * Getter for the full name of a team member
+   * @return the full name
+   */
   public String getFullName()
   {
     return firstName + " " + lastName;
   }
 
+  /**
+   * Getter for the formal name of a team member
+   * @return the formal name
+   */
   public String getFormalName()
   {
     return lastName + " " + firstName;
   }
 
+  /**
+   * Setter for the name of a team member
+   * @param firstName the first name
+   * @param lastName the last time
+   */
   public void setName(String firstName, String lastName)
   {
     this.firstName = firstName;
     this.lastName = lastName;
   }
 
+  /**
+   * Checking if two names are the same
+   * @param obj object to be compared to
+   * @return a boolean indicating if two instances are similar
+   */
   public boolean equals(Object obj)
   {
     if (!(obj instanceof Name))
@@ -46,6 +80,10 @@ public class Name
     return firstName.equals(other.firstName) && lastName.equals(other.lastName);
   }
 
+  /**
+   * Getting the team member's name
+   * @return a string returning the full name of a team member
+   */
   @Override public String toString()
   {
     return getFullName();
