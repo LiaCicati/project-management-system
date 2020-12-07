8032f824aa6ff29252dd463fc985633b029470bd
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-07 11:55:23 +0100

    add some files

---
+++ SingleUserSystem/src/Mediator/ProjectManagementFile.java
@@ -0,0 +1,13 @@
+package Mediator;
+
+import java.io.File;
+
+public class ProjectManagementFile
+{
+  private File file;
+
+  public ProjectManagementFile(String fileName)
+  {
+    fileName=file.getName();
+  }
+}
