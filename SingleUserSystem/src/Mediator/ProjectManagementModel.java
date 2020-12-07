d27ab8b6b81cdc704b8c38dc63e7c5ae8589c0db
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-07 11:52:48 +0100

    Update ProjectManagementModel.java

--- SingleUserSystem/src/Mediator/ProjectManagementModel.java
+++ SingleUserSystem/src/Mediator/ProjectManagementModel.java
@@ -1,5 +1,35 @@
 package Mediator;
 
+import Model.*;
+import javafx.scene.Scene;
+
 public interface ProjectManagementModel
 {
+  public TeamMemberList getTeamMember(String role);
+
+  public void deleteTask(Task task);
+
+  public void addProject(Project project);
+
+  public void deleteTeamMember(TeamMember teamMember);
+
+  public TaskList getAllTasks();
+
+  public ProjectList getAllProjects();
+
+  public void addRequirement(Requirement requirement);
+
+  public Project getProject(String title);
+
+  public void addTask(Task task);
+
+  public void addTeamMember(TeamMember teamMember);
+
+  public TeamMemberList getAllTeamMembers();
+
+  public void deleteRequirement(Requirement requirement);
+
+  public RequirementList getAllRequirements();
+
+  public TeamMember addTeamMember(Name name,String role);
 }
