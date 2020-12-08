cc34f52c9e702d66752f392043a9436c1ecb8acb
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-08 11:04:07 +0100

    Update ProjectManagementModel.java

--- SingleUserSystem/src/Mediator/ProjectManagementModel.java
+++ SingleUserSystem/src/Mediator/ProjectManagementModel.java
@@ -1,35 +1,45 @@
 package Mediator;
 
 import Model.*;
-import javafx.scene.Scene;
 
 public interface ProjectManagementModel
 {
-  public TeamMemberList getTeamMember(String role);
-
-  public void deleteTask(Task task);
-
-  public void addProject(Project project);
-
-  public void deleteTeamMember(TeamMember teamMember);
-
-  public TaskList getAllTasks();
-
-  public ProjectList getAllProjects();
-
-  public void addRequirement(Requirement requirement);
-
-  public Project getProject(String title);
-
-  public void addTask(Task task);
+  public TeamMemberList getTeamMemberByName(Name name);
 
   public void addTeamMember(TeamMember teamMember);
 
-  public TeamMemberList getAllTeamMembers();
+  public void removeTeamMember(TeamMember teamMember);
 
-  public void deleteRequirement(Requirement requirement);
+  public void removeTeamMemberFromRequirement(Requirement requirement,TeamMember teamMember);
 
-  public RequirementList getAllRequirements();
+  public void addTaskToRequirement(Requirement requirement,Task task);
+
+  public void removeTaskFromRequirement(Requirement requirement,Task task);
+
+  public void addRequirementToProject(Project project,Requirement requirement);
+
+  public void removeRequirementFromProject( Project project,Requirement requirement);
+
+  public void addProject(Project project);
+
+  public void removeProject(Project project);
+
+  public TeamMemberList getAllTeamMembers(TeamMemberList teamMembers);
+
+  public TaskList getAllTasks(TaskList tasks);
+
+  public RequirementList getAllRequirements(RequirementList requirements);
+
+  public ProjectList getAllProjects(ProjectList projects);
+
+  public Project getProject(String title);
+
+  public void reorder(int position, int newPosition);
 
   public TeamMember addTeamMember(Name name,String role);
+
+  public int getSize();
+
+  public TeamMember get(int index);
+
 }
