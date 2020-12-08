f435820d021f115a23bd939743d319bd351f94d8
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-08 11:47:35 +0100

    Update ProjectManagementModelManager.java

--- SingleUserSystem/src/Mediator/ProjectManagementModelManager.java
+++ SingleUserSystem/src/Mediator/ProjectManagementModelManager.java
@@ -2,7 +2,8 @@ package Mediator;
 
 import Model.*;
 
-import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ProjectManagementModelManager
 {
@@ -20,19 +21,16 @@ public class ProjectManagementModelManager
     this.teamMemberList = new TeamMemberList();
   }
 
-  public void addProject(Project project)
+  public TeamMember getTeamMemberByName(Name name)
   {
-    projectList.addProject(project);
-  }
-
-  public void addRequirement(Requirement requirement)
-  {
-    requirementList.addRequirement(requirement);
-  }
-
-  public void addTask(Task task)
-  {
-    taskList.addTask(task);
+    for(int i=0;i<teamMemberList.getSize();i++)
+    {
+      if(teamMemberList.get(i).getName().equals(name))
+      {
+        return teamMemberList.get(i);
+      }
+    }
+    return null;
   }
 
   public void addTeamMember(TeamMember teamMember)
@@ -40,9 +38,51 @@ public class ProjectManagementModelManager
     teamMemberList.addTeamMember(teamMember);
   }
 
-  public void deleteRequirement(Requirement requirement)
+  public void editTeamMember(Name name,String role)
   {
-    requirementList.removeRequirement(requirement);
+    for(int i=0;i<teamMemberList.getSize();i++)
+    {
+      if(teamMemberList.get(i).getName().equals(name))
+      {
+        teamMemberList.get(i).getRole().equals(role);
+      }
+    }
+  }
+
+
+  public void removeTeamMember(TeamMember teamMember)
+  {
+    teamMemberList.removeTeamMember(teamMember);
+  }
+
+  public void addTaskToRequirement(Requirement requirement,Task task)
+  {
+    requirement.getAllTasks().add(task);
+  }
+
+  public void removeTaskFromRequirement(Requirement requirement,Task task)
+  {
+    requirement.getAllTasks().remove(task);
+  }
+
+  public void addRequirementToProject(Project project,Requirement requirement)
+  {
+    project.getAllRequirements().add(requirement);
+  }
+
+  public void removeRequirementFromProject( Project project,Requirement requirement)
+  {
+    project.getAllRequirements().remove(requirement);
+  }
+
+  public void addProject(Project project)
+  {
+    projectList.addProject(project);
+  }
+
+  public void removeProject(Project project)
+  {
+    projectList.removeProject(project);
   }
 
   public void deleteTask(Task task)
@@ -50,9 +90,19 @@ public class ProjectManagementModelManager
     taskList.removeTask(task);
   }
 
-  public void deleteTeamMember(TeamMember teamMember)
+  public ArrayList<TeamMember> getAllTeamMembers(Project project)
   {
-    teamMemberList.removeTeamMember(teamMember);
+    return project.getAllTeamMembers();
+  }
+
+  public ArrayList<Task> getAllTasks(Requirement requirement)
+  {
+    return requirement.getAllTasks();
+  }
+
+  public ArrayList<Requirement> getAllRequirements(Project project)
+  {
+    return project.getAllRequirements();
   }
 
   public ProjectList getAllProjects()
@@ -60,25 +110,45 @@ public class ProjectManagementModelManager
     return projectList;
   }
 
-  public TeamMemberList getAllTeamMembers()
-  {
-    return teamMemberList;
-  }
-
-  public RequirementList getAllRequirements()
-  {
-    return requirementList;
-  }
-
-  public TaskList getAllTasks()
-  {
-    return taskList;
-  }
-
   public Project getProject(String title)
   {
     return projectList.getProjectByTitle(title);
   }
 
-  //public TeamMemberList getTeamMember(String role)  (this method is in the diagram,but i think we don't need it anymore)
+//  public void reorder(int position, int newPosition)
+//  {
+//    try
+//    {
+//      Collections.swap(requirements, position, newPosition);
+//    }
+//    catch (IndexOutOfBoundsException e)
+//    {
+//      e.printStackTrace();
+//    }
+//  }
+
+  public int getSizeOfTask(Requirement requirement,Task task)
+  {
+    return requirement.getAllTasks().size();
+  }
+
+  public int getSizeOfRequirement(Project project,Requirement requirement)
+  {
+    return project.getAllRequirements().size();
+  }
+
+  public int getSizeOfProject(Project project)
+  {
+    return projectList.getAllProjects().size();
+  }
+
+  public TeamMember get(Project project,int index)
+  {
+    return project.getAllTeamMembers().get(index);
+  }
+
+  public TeamMember getTeamMemberById(Project project,int ID)
+  {
+    return project.getTeamMemberByID(ID);
+  }
 }
