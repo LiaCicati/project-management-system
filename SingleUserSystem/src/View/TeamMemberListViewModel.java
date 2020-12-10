8dcfe4e1a6622811cdd81a64724b370496078fd8
Siyu <59789515+TROY571@users.noreply.github.com>
2020-12-10 14:41:56 +0100

    Update TeamMemberListViewModel.java

--- SingleUserSystem/src/View/TeamMemberListViewModel.java
+++ SingleUserSystem/src/View/TeamMemberListViewModel.java
@@ -1,49 +1,51 @@
-//package View;
-//
-//import Mediator.ProjectManagementModel;
-//import Mediator.TeamMemberListModel;
-//import javafx.collections.FXCollections;
-//import javafx.collections.ObservableList;
-//import Model.*;
-//
-//public class TeamMemberListViewModel
-//{
-//  private ObservableList<TeamMemberViewModel> list;
-//  private TeamMemberListModel model;
-//
-//  public TeamMemberListViewModel(TeamMemberListModel model)
-//  {
-//    this.model=model;
-//    this.list= FXCollections.observableArrayList();
-//    update();
-//  }
-//
-//  public ObservableList<TeamMemberViewModel> getList(){return list;}
-//
-//  public void update()
-//  {
-//    list.clear();
-//    for(int i=0;i< model.teamMemberListSize();i++)
-//    {
-//      list.add(new TeamMemberViewModel(model.getTeamMember(i)));
-//    }
-//  }
-//
-//  public void remove(TeamMember teamMember)
-//  {
-//    for (int i = 0; i < list.size(); i++)
-//    {
-//      if (list.get(i).getNameProperty().get().equals(teamMember.getName())
-//          && list.get(i).getIdProperty().get() == teamMember.getId())
-//      {
-//        list.remove(i);
-//        break;
-//      }
-//    }
-//  }
-//
-//  public void add(TeamMember teamMember)
-//  {
-//    list.add(new TeamMemberViewModel(teamMember));
-//  }
-//}
+package View;
+
+import Mediator.ProjectManagementModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import Model.*;
+
+public class TeamMemberListViewModel
+{
+  private ObservableList<TeamMemberViewModel> list;
+  private ProjectManagementModel model;
+  private ViewState viewState;
+
+
+  public TeamMemberListViewModel(ProjectManagementModel model,ViewState viewState)
+  {
+    this.model=model;
+    this.list= FXCollections.observableArrayList();
+    this.viewState=viewState;
+    update();
+  }
+
+  public ObservableList<TeamMemberViewModel> getList(){return list;}
+
+  public void update()
+  {
+    list.clear();
+    for(int i=0;i< model.getNumberOfTeamMembers();i++)
+    {
+      list.add(new TeamMemberViewModel(model.getTeamMemberById(i)));
+    }
+  }
+
+  public void remove(TeamMember teamMember)
+  {
+    for (int i = 0; i < list.size(); i++)
+    {
+      if (list.get(i).getNameProperty().get().equals(teamMember.getName())
+          && list.get(i).getIdProperty().get() == teamMember.getId())
+      {
+        list.remove(i);
+        break;
+      }
+    }
+  }
+
+  public void add(TeamMember teamMember)
+  {
+    list.add(new TeamMemberViewModel(teamMember));
+  }
+}
