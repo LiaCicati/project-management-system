b3378c01b89f4728ae12e6e1bf2f0bec71a1bb2c
Lia <liacicati@gmail.com>
2020-12-11 16:41:34 +0100

    remove teamMember button & design change for requirement tab

--- SingleUserSystem/src/View/TeamMemberListViewModel.java
+++ SingleUserSystem/src/View/TeamMemberListViewModel.java
@@ -7,53 +7,55 @@ import Model.*;
 
 public class TeamMemberListViewModel
 {
-    private ObservableList<TeamMemberViewModel> list;
-    private ProjectManagementModel model;
-    private ViewState viewState;
+  private ObservableList<TeamMemberViewModel> list;
+  private ProjectManagementModel model;
+  private ViewState viewState;
 
-    public TeamMemberListViewModel(ProjectManagementModel model,
-        ViewState viewState)
-    {
-        this.model = model;
-        this.list = FXCollections.observableArrayList();
-        this.viewState = viewState;
-        update();
-    }
+  public TeamMemberListViewModel(ProjectManagementModel model,
+      ViewState viewState)
+  {
+    this.model = model;
+    this.list = FXCollections.observableArrayList();
+    this.viewState = viewState;
+    update();
+  }
 
-    public ObservableList<TeamMemberViewModel> getList()
-    {
-        return list;
-    }
+  public ObservableList<TeamMemberViewModel> getList()
+  {
+    return list;
+  }
 
-    public void update()
+  public void update()
+  {
+    list.clear();
+    for (int i = 0; i < model.getTeamMemberList(
+        model.getAllProjects().getProjectById(viewState.getSelectedProject()))
+        .getSize(); i++)
     {
-        list.clear();
-        for (int i = 0; i < model.getTeamMemberList(model.getAllProjects()
-            .getProjectById(viewState.getSelectedProject())).getSize(); i++)
-        {
-            list.add(new TeamMemberViewModel(model.getTeamMemberList(
-                model.getAllProjects()
-                    .getProjectById(viewState.getSelectedProject())).get(i)));
-        }
+      list.add(new TeamMemberViewModel(model.getTeamMemberList(
+          model.getAllProjects().getProjectById(viewState.getSelectedProject()))
+          .get(i)));
     }
+  }
 
-    public void remove(TeamMember teamMember)
+  public void remove(TeamMember teamMember)
+  {
+    for (int i = 0; i < list.size(); i++)
     {
-        for (int i = 0; i < list.size(); i++)
-        {
-            if (list.get(i).getNameProperty().equals(teamMember.getName())
-                && list.get(i).getIdProperty().get() == teamMember.getId()
-                && list.get(i).getRoleProperty().get()
-                .equals(teamMember.getRole()))
-            {
-                list.remove(i);
-                break;
-            }
-        }
+      if (list.get(i).getNameProperty().getValue()
+          .equals(teamMember.getName().toString())
+          && list.get(i).getIdProperty().getValue() == teamMember.getId()
+          && list.get(i).getRoleProperty().getValue()
+          .equals(teamMember.getRole()))
+      {
+        list.remove(i);
+        break;
+      }
     }
+  }
 
-    public void add(TeamMember teamMember)
-    {
-        list.add(new TeamMemberViewModel(teamMember));
-    }
+  public void add(TeamMember teamMember)
+  {
+    list.add(new TeamMemberViewModel(teamMember));
+  }
 }
