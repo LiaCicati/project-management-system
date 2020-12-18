391051cf688f0c82988767a473ef427f64853aff
Rickie Nielsen <nielsenrickie@gmail.com>
2020-12-18 04:17:25 +0100

    added javadoc comments

--- SingleUserSystem/src/Mediator/ProjectManagementModelManager.java
+++ SingleUserSystem/src/Mediator/ProjectManagementModelManager.java
@@ -26,16 +26,21 @@ public class ProjectManagementModelManager implements ProjectManagementModel
     private RequirementList requirementList;
     private TaskList taskList;
 
+    /**
+     * Constructor for modelManager
+     *
+     * @throws ParserException exception needed fir initProjectsFromFile method
+     */
     public ProjectManagementModelManager() throws ParserException
     {
         this.projectList = new ProjectList();
         this.teamMemberList = new TeamMemberList();
         initProjectsFromFiles();//calls the method to load the .xml files
-            createDummyData();
+        createDummyData();
     }
 
-    /**method that finds and displays .xml files
-     *
+    /**
+     * method that finds and displays .xml files
      */
     private void initProjectsFromFiles()
     {
@@ -60,26 +65,32 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
-    /**Method to read from .xml file
+    /**
+     * Method to read from .xml file
      *
      * @param fileName name of the file to read
      * @return parsed version of .xml file
-     * @throws ParserException needed exception
+     * @throws ParserException       needed exception
      * @throws IllegalStateException
      */
-    private Project getSingleProject(String fileName) throws ParserException, IllegalStateException
+    private Project getSingleProject(String fileName)
+        throws ParserException, IllegalStateException
     {
         XmlJsonParser parser = new XmlJsonParser();
         return parser.fromXml(fileName, Project.class);
     }
 
-
-      private void createDummyData()
-      {
-        Project project1 = new Project("Goldcar Rental Company", new MyDate(18, 12, 2020),
-            353, "Needs a reservation system to handle customers, vehicles and reservations.");
+    /**
+     * Creating Dummy data
+     */
+    private void createDummyData()
+    {
+        Project project1 = new Project("Goldcar Rental Company",
+            new MyDate(18, 12, 2020), 353,
+            "Needs a reservation system to handle customers, vehicles and reservations.");
         Project project2 = new Project("IT Minds", new MyDate(23, 12, 2020),
-            338, "Needs a project management system to handle tasks and time for their IT projects in order to increase the company’s productivity and not waste time on little tasks");
+            338,
+            "Needs a project management system to handle tasks and time for their IT projects in order to increase the company’s productivity and not waste time on little tasks");
         projectList.addProject(project1);
         projectList.addProject(project2);
 
@@ -109,9 +120,13 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         project2.addRequirement(requirement2);
         project2.addTeamMember(teamMember1);
 
+    }
 
-      }
-
+    /**
+     * Getter for a team member by name
+     * @param name name of team member
+     * @return team member with specified name
+     */
     @Override public TeamMember getTeamMemberByName(Name name)
     {
         for (Project project : projectList.getAllProjects())
@@ -128,6 +143,12 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         return null;
     }
 
+    /**
+     * adding a task to a requirement
+     * @param requirementID id of requirement related to the task you wish to add
+     * @param projectID id of project related to the requirement
+     * @param task task to be added
+     */
     @Override public void addTask(int requirementID, int projectID, Task task)
     {
         projectList.getProjectById(projectID).getAllRequirements()
@@ -135,11 +156,23 @@ public class ProjectManagementModelManager implements ProjectManagementModel
 
     }
 
+    /**
+     * Adding a team member to a project
+     * @param projectID id of project a team member will be added to
+     * @param teamMember team member to be added
+     */
     @Override public void addTeamMember(int projectID, TeamMember teamMember)
     {
         projectList.getProjectById(projectID).addTeamMember(teamMember);
     }
 
+    /**
+     * edit values for a team member
+     * @param projectID id of project related to the team member
+     * @param ID id of team member
+     * @param teamMember team member to be edited
+     * @param role role of team member
+     */
     @Override public void editTeamMember(int projectID, int ID,
         TeamMember teamMember, String role)
     {
@@ -150,6 +183,10 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             .editTeamMember(teamMember.getName(), teamMember.getId());
     }
 
+    /**
+     * removing a team member from a project
+     * @param teamMember team member to be removed
+     */
     @Override public void removeTeamMember(TeamMember teamMember)
     {
         for (Project project : projectList.getAllProjects())
@@ -161,6 +198,11 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
+    /**
+     * adding a task to a requirement
+     * @param requirement requirement a task will be added to
+     * @param task task to be added
+     */
     @Override public void addTaskToRequirement(Requirement requirement,
         Task task)
     {
@@ -177,6 +219,11 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
+    /**
+     * removing a task from a requirement
+     * @param requirement requirement related to the task
+     * @param task task to be removed
+     */
     @Override public void removeTaskFromRequirement(Requirement requirement,
         Task task)
     {
@@ -193,6 +240,11 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
+    /**
+     * removing a team member from a task
+     * @param task task related to the team member
+     * @param teamMember team member to be removed
+     */
     @Override public void removeTeamMemberFromTask(Task task,
         TeamMember teamMember)
     {
@@ -212,11 +264,21 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
+    /**
+     * adding a requirement to a project
+     * @param projectID id of project the requirement will be added to
+     * @param requirement requirement to be added
+     */
     @Override public void addRequirement(int projectID, Requirement requirement)
     {
         projectList.getProjectById(projectID).addRequirement(requirement);
     }
 
+    /**
+     * removing a requirement from a project
+     * @param project project related to the requirement
+     * @param requirementID id of requirement to be removed
+     */
     @Override public void removeRequirementFromProject(Project project,
         int requirementID)
     {
@@ -229,31 +291,62 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         }
     }
 
+    /**
+     * adding a project to the list
+     * @param project project to be added
+     * @param title project title
+     * @param ID customer id
+     */
     @Override public void addProject(Project project, String title, int ID)
     {
         projectList.addProject(project);
     }
 
+    /**
+     * removing a project
+     * @param project project to be removed
+     */
     @Override public void removeProject(Project project)
     {
         projectList.removeProject(project);
     }
 
+    /**
+     * getter for all team members
+     * @param project project you wish to get team members from
+     * @return all team members for the specified project
+     */
     @Override public TeamMemberList getAllTeamMembers(Project project)
     {
         return project.getAllTeamMembers();
     }
 
+    /**
+     * getter for all tasks
+     * @param requirement requirement you wish to get all tasks from
+     * @return all tasks from the specified requirement
+     */
     @Override public TaskList getAllTasks(Requirement requirement)
     {
         return requirement.getAllTasks();
     }
 
+    /**
+     * getter for all requirements
+     * @param project project you wish to get all requirements from
+     * @return all requirements from specified project
+     */
     @Override public RequirementList getAllRequirements(Project project)
     {
         return project.getAllRequirements();
     }
 
+    /**
+     * getter for all tasks
+     * @param project project related to the tasks
+     * @param requirement requirement related to the tasks
+     * @return all tasks from related requirement and project
+     */
     @Override public TaskList getAllTasks(Project project,
         Requirement requirement)
     {
@@ -265,6 +358,13 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             "Could not find any tasks belonging to this requirement");
     }
 
+    /**
+     * getter for all team members
+     * @param project project related to team members
+     * @param requirement requirement related to team members
+     * @param task task related to team members
+     * @return all team members related to the specified project, requirement and task
+     */
     @Override public TeamMemberList getAllTeam(Project project,
         Requirement requirement, Task task)
     {
@@ -277,42 +377,82 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             "Could not find any team members belonging to this task");
     }
 
+    /**
+     *getter for all projects
+     * @return all projects
+     */
     @Override public ProjectList getAllProjects()
     {
         return projectList;
     }
 
+    /**
+     * getter for a project
+     * @param title project title
+     * @return project with specified title
+     */
     @Override public Project getProject(String title)
     {
         return projectList.getProjectByTitle(title);
     }
 
+    /**
+     * reorder priority of requirements
+     * @param project project related to requirements
+     * @param position position you wish to swap
+     * @param newPosition position you wish to swap to
+     */
     @Override public void reorderRequirements(Project project, int position,
         int newPosition)
     {
         project.getAllRequirements().reorder(position, newPosition);
     }
 
+    /**
+     * getter for number of tasks
+     * @param requirement requirement related to tasks
+     * @return number of tasks for the requirement
+     */
     @Override public int getNumberOfTasks(Requirement requirement)
     {
         return requirement.countTasks();
     }
 
+    /**
+     * getter for number of requirements
+     * @param project project related to requirements
+     * @return number of requirements for the project
+     */
     @Override public int getNumberOfRequirements(Project project)
     {
         return project.getNumberOfRequirements();
     }
 
+    /**
+     * getter for number of projects
+     * @return number of projects
+     */
     @Override public int getNumberOfProjects()
     {
         return projectList.getNumberOfProjects();
     }
 
+    /**
+     * getter for team member
+     * @param project project related to team member
+     * @param index index you wish to get
+     * @return team member at index
+     */
     @Override public TeamMember get(Project project, int index)
     {
         return project.getAllTeamMembers().get(index);
     }
 
+    /**
+     * getter for team member by an id
+     * @param ID id of team member
+     * @return team member with specified id
+     */
     @Override public TeamMember getTeamMemberById(int ID)
     {
         for (int i = 0; i < teamMemberList.getSize(); i++)
@@ -325,6 +465,14 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         return null;
     }
 
+    /**
+     * edit values for a task
+     * @param task task to edit
+     * @param taskID id of task
+     * @param projectID project id related to task
+     * @param requirementID requirement id related to task
+     * @param status status of task
+     */
     @Override public void editTask(Task task, int taskID, int projectID,
         int requirementID, String status)
     {
@@ -338,6 +486,13 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             .setStatus(status);
     }
 
+    /**
+     * edit values for a requirement
+     * @param projectID project id related to requirement
+     * @param ID requirement id
+     * @param requirement requirement to edit
+     * @param status requirement status
+     */
     @Override public void editRequirement(int projectID, int ID,
         Requirement requirement, String status)
     {
@@ -350,16 +505,35 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             .setStatus(status);
     }
 
+    /**
+     * change status of a project
+     * @param project project
+     * @param status status for project
+     */
     @Override public void changeStatus(Project project, String status)
     {
         project.changeStatus(status);
     }
 
+    /**
+     * change role of team member
+     * @param teamMember team member
+     * @param role role of team member
+     */
     @Override public void changeRole(TeamMember teamMember, String role)
     {
         teamMember.changeRole(role);
     }
 
+    /**
+     * edit values of a project
+     * @param projectID id of project
+     * @param title project title
+     * @param customerID id of a projects customer
+     * @param description project description
+     * @param deadline project deadline
+     * @param status project status
+     */
     @Override public void editProject(int projectID, String title,
         int customerID, String description, MyDate deadline, String status)
     {
@@ -368,42 +542,83 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         projectList.getProjectById(projectID).changeStatus(status);
     }
 
+    /**
+     * getter for a project
+     * @param index index of projectList
+     * @return the project at the given index
+     */
     public Project getProject(int index)
     {
         return projectList.getProject(index);
     }
 
+    /**
+     * getter for team member
+     * @param projectID project id related to team member
+     * @param index index to get
+     * @return team member at index
+     */
     @Override public TeamMember getTeamMemberAtIndex(int projectID, int index)
     {
         return projectList.getProjectById(projectID)
             .getTeamMemberAtIndex(index);
     }
 
+    /**
+     * getter for a project
+     * @param id project id
+     * @return the project with specified id
+     */
     @Override public Project getProjectByID(int id)
     {
         return projectList.getProjectById(id);
     }
 
+    /**
+     * getter for team members
+     * @return all team members
+     */
     @Override public TeamMemberList getTeamMembers()
     {
         return teamMemberList;
     }
 
+    /**
+     * getter for number of team members
+     * @return number of team members
+     */
     public int getNumberOfTeamMembers()
     {
         return teamMemberList.getSize();
     }
 
+    /**
+     * getter for all requirements
+     * @return all requirements
+     */
     public RequirementList getAllRequirements()
     {
         return requirementList;
     }
 
+    /**
+     * getter for all team members
+     * @param project project related to team members
+     * @return all team members for the specified project
+     */
     public TeamMemberList getTeamMemberList(Project project)
     {
         return project.getAllTeamMembers();
     }
 
+    /**
+     * getter for a team member
+     * @param projectID project id related to team members
+     * @param requirementID requirement id related to team members
+     * @param taskID task id related to team members
+     * @param teamMemberID team member id
+     * @return team member with given id
+     */
     @Override public TeamMember getAllTeamMembers(int projectID,
         int requirementID, int taskID, int teamMemberID)
     {
@@ -412,6 +627,11 @@ public class ProjectManagementModelManager implements ProjectManagementModel
             .getAllTeamMembers().getTeamMemberById(teamMemberID);
     }
 
+    /**
+     * getter for a requirement
+     * @param id requirement id
+     * @return the requirement with given id
+     */
     @Override public Requirement getRequirementByID(int id)
     {
         for (int i = 0; i < requirementList.getSize(); i++)
@@ -424,6 +644,14 @@ public class ProjectManagementModelManager implements ProjectManagementModel
         return null;
     }
 
+    /**
+     * add hours worked to a task
+     * @param projectID id related to a project
+     * @param requirementID reuirement id
+     * @param taskID task id
+     * @param teamMemberID team member id
+     * @param hours hours worked
+     */
     @Override public void addHours(int projectID, int requirementID, int taskID,
         int teamMemberID, double hours)
     {
